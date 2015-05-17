31462842053196f2f986f22fa1f4ba01ea97cbe9
brandynstroud1 <b2stroud@fullsail.edu>
2015-05-16 20:19:11 -0400

    Lab6 functions myArray(){}

---
+++ Lab6/lab6.js
@@ -0,0 +1,19 @@
+/**
+ * Created by brandynstroud1 on 5/16/15.
+ */
+
+
+// Arrays
+
+function myArrays(){
+
+    var gameCharacters  = ["Gex", "Eddy Gordo", "Spyro", "Nathan Drake"];
+
+    for (var i = 0; i < gameCharacters.length; i++){
+
+            console.log(gameCharacters[i] + " is my favorite video game character.");
+    }
+
+}
+
+myArrays();
