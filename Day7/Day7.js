8b26b39c9c8b732e2ea92dd4d1d16acd23b32ab2
brandynstroud1 <b2stroud@fullsail.edu>
2015-05-19 09:21:16 -0400

    Lab 6 review. Day 7 beginning

---
+++ Day7/Day7.js
@@ -0,0 +1,20 @@
+/**
+ * Created by brandynstroud1 on 5/19/15.
+ */
+
+//alert("JavaScript Works.");
+
+//Lab6 review
+
+//function
+function authorFunction(){
+
+    //local arrays
+
+    // for loop to go through the array items
+    // output each element
+
+    //use an array method to alter the arrays
+
+    //do a final output of the
+}
