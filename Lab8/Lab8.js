d6daa0ec239e9debd1b1ce40ef7c82c59a19d6a9
brandynstroud1 <b2stroud@fullsail.edu>
2015-05-21 15:24:47 -0400

    Lab8

---
+++ Lab8/Lab8.js
@@ -0,0 +1,25 @@
+/**
+ * Created by brandynstroud1 on 5/21/15.
+ */
+
+//Brandyn Stroud
+//5-21-2015
+//SDI Lab 8
+//Working with Strings
+
+//global variable
+var myEmail = "b2stroud@fullsail.iii";
+
+//function
+function newEmail(emailArg) {
+
+    //local variables
+    var atSymbol = emailArg.indexOf("b2stroud@fullsail.iii");
+
+    return atSymbol
+
+}
+
+newEmail(myEmail);
+
+prompt("Enter your email:", "b2stroud@fullsail.edu.iii");
