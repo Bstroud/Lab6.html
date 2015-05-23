8077977ffba57f6f32bbbcf26f403107bc8ac09c
brandynstroud1 <b2stroud@fullsail.edu>
2015-05-23 13:53:19 -0400

    Lab 9 Problem 1

---
+++ sdi-lab9/Lab9.js
@@ -0,0 +1,25 @@
+//Brandyn Stroud
+//SDI Lab 9
+//Number and Date Objects
+//5-23-2015
+
+//global variables
+var NumString;
+
+//function
+function number(theNumber) {
+
+    var wholeNum = 30;
+    var deciNum = 15.5;
+
+
+console.log("Numbers so far: " + wholeNum + deciNum);
+
+
+
+}
+
+
+
+//main code
+number();
