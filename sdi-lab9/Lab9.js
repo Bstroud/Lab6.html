14305203dd6b21dac7443a54edcc6d37dc0f1078
brandynstroud1 <b2stroud@fullsail.edu>
2015-05-23 23:26:17 -0400

    Lab 9 Final Commit

--- sdi-lab9/Lab9.js
+++ sdi-lab9/Lab9.js
@@ -9,51 +9,51 @@
 
 
 
-////Problem 1
-//
-////global variables
-//var NumString;
-//
-////function
-//function number(theNumber) {
-//
-//    var num = 3.4143;
-//    var n = num.toPrecision(4);
-//
-//
-//    console.log("Answer equals " + n);
-//
-//}
-//
-//
-//
-////main code
-//number();
+//Problem 1
+
+//global variables
+var NumString;
+
+//function
+function number(theNumber) {
+
+    var num = 3.4143;
+    var n = num.toPrecision(4);
+
+
+    console.log("Answer equals " + n);
+
+}
+
+
+
+//main code
+number();
 
 
 
 
-//// Problem 2
-//
-////global variable
-//var shoeSize;
-//
-//// function
-//
-//function size(numberSize){
-//
-//    var lengthOfFoot = prompt("Enter your shoe size: ");
-//
-//    for (i = 0; i < lengthOfFoot.length; i++){
-//        if (lengthOfFoot[i] > 0 ){
-//            console.log("Thanks you for your submission");
-//        } else {
-//            console.log("Please enter a number.")
-//        }
-//    }
-//}
-//
-//size();
+// Problem 2
+
+//global variable
+var shoeSize;
+
+// function
+
+function size(numberSize){
+
+    var lengthOfFoot = prompt("Enter your shoe size: ");
+
+    for (i = 0; i < lengthOfFoot.length; i++){
+        if (lengthOfFoot[i] > 0 ){
+            console.log("Thanks you for your submission");
+        } else {
+            console.log("Please enter a number.")
+        }
+    }
+}
+
+size();
 
 //Problem 3
 
