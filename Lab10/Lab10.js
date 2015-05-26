8f888a9843c9dfaf3d8912953e3c6b030a80fabd
brandynstroud1 <b2stroud@fullsail.edu>
2015-05-26 14:32:11 -0400

    Stroud_Brandyn_Lab10

---
+++ Lab10/Lab10.js
@@ -0,0 +1,25 @@
+/**
+ * Created by brandynstroud1 on 5/26/15.
+ */
+
+//Brandyn Stroud
+//SDI Lab 10
+//Problem-Solving
+//5-26-2015
+
+//global var
+var numArray = Math.random();
+var returnedRandom;
+
+
+//function
+function randomNum(numbers) {
+
+
+
+
+}
+
+
+//main code
+returnedRandom = randomNum(numArray);
